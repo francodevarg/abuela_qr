0bef58e1190df5e2829dde1812a353e2fea6e07c
Franco Narvaez <juanfranconarvaez@gmail.com>
2023-12-28 16:19:52 -0300

    Domains for Images and Fonts are setted
    Cloudinary and GoogleFonts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['res.cloudinary.com', 'fonts.googleapis.com'],
+      },
+}
 
 module.exports = nextConfig
