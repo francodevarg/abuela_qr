fbfc3ca0a213c795c647be435b3f902b2b169ad1
Franco Narvaez <juanfranconarvaez@gmail.com>
2024-01-05 10:26:23 -0300

    favicon and metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Mis 80',
+  title: 'Mis 80 | Cristina Pazos',
   description: 'Cristina Pazos',
 }
 
